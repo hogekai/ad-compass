b2e6eaaeed36cdd153f59e11b573c9acf1fd4651
Kai <kaihogehoge@gmail.com>
2024-08-25 02:36:38 +0900

    Add test for should emit events

---
+++ tests/unit/EventEmitter.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it, vi } from "vitest";
+import { EventEmitter } from "@/EventEmitter";
+import { AdCompassEventType } from "@/types/AdCompassEventType";  
+
+describe('EventEmitter', () => {
+    it('should emit events', () => {
+        const emitter = new EventEmitter();
+
+        const callback = vi.fn();
+        emitter.on(AdCompassEventType.ALTERNATIVE_CONTENT_IMPRESSION, callback);
+        emitter.emit(AdCompassEventType.ALTERNATIVE_CONTENT_IMPRESSION, null);
+
+        expect(callback).toHaveBeenCalled();        
+    });
+
+    it('should listen to events', () => {
+        // test code
+    });
+
+    it('should remove listeners', () => {
+        // test code
+    }); 
+});
