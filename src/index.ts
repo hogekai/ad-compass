fba9e83ce2c4b92b93dbc6567d31efb058a243b4
Kai <kaihogehoge@gmail.com>
2024-08-26 03:05:55 +0900

    Modification of module structure

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { ErrorCode } from "./types/ErrorCode";
 import { AppendChildStrategy, InsertBeforeStrategy } from "./placement-strategy/";
 import { HTMLAlternativeContent, ImageAlternativeContent } from "./alternative-content";
 import { AlternativeContentPlacer } from "./AlternativeContentPlacer";
+import { PlacementStrategy } from "./PlacementStrategy";
+import { AlternativeContent } from "./AlternativeContent";
 
 export {
   AdCompassEventType,
@@ -12,7 +14,12 @@ export {
   InsertBeforeStrategy,
   HTMLAlternativeContent,
   ImageAlternativeContent,
-  AlternativeContentPlacer
+  AlternativeContentPlacer,
 };
+
+export type {
+  PlacementStrategy,
+  AlternativeContent,
+}
 
 export default AdCompass;
